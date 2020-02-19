c26ad24077a3c14dcb3684651dd77b149309c3d2
Rebekah Gennick <rebekahgennick92@gmail.com>
2020-02-19 04:37:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def line(katz_deli)
+  katz_deli= []
+  if katz_deli < 1
+    puts "This line is currently empty. "
+    katz_deli +=1 
+    katz_deli.each_with_index { |name, i| puts #{name}, #{index}}
+  end
+  return katz_deli
+  end

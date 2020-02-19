7d8058bbd60b8fb63e42360fa6526dbbbf8d1010
Rebekah Gennick <rebekahgennick92@gmail.com>
2020-02-19 04:42:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 def line(katz_deli)
   katz_deli= []
-  if katz_deli < 1
+  katz_deli < 1
     puts "This line is currently empty. "
     katz_deli +=1 
     katz_deli.each_with_index { |name, i| puts #{name}, #{index}}
